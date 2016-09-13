324dd67bd01cd13ede1881370999f9154e9b9083
Jason Walsh <j@wal.sh>
2016-09-13 15:48:53 -0400

    Log review

--- src/ao.js
+++ src/ao.js
@@ -9,7 +9,8 @@
 export default function ao(ua = window.navigator.userAgent) {
     'use strict';
     // https://support.google.com/webmasters/answer/1061943?hl=en
-    let BOTS_RE=/AhefsBot|Baiduspider|bingbot|Googlebot|MJ12bot|YandexBot|Crawler/;
+    let BOTS_RE=/60Spider|AhrefsBot|Baiduspider|Baiduspider-image|DuckDuckGo-Favicons-Bot|Exabot|Feedfetcher-Google|Feedfetcher-Google|Googlebot|Googlebot|MJ12bot|Mail.RU_Bot|PhantomJS|SAMSUNG-SGH-E250|SafeDNSBot|SemrushBot|Seznam screenshot-generator|SeznamBot|Sogou web spider|Uptimebot|Yahoo! Slurp|YandexBot|bingbot|linkdexbot|spbot/;
+
     // https://developer.chrome.com/multidevice/user-agent
     let MOBILES_RE=/iPhone|iPad|iPod|Android/;
     return ua.search(MOBILES_RE);
