0ce518524c9841e561c5a80db30e0de525bc75f2
Jason Walsh <j@wal.sh>
2016-09-14 14:49:03 -0400

    Base tests

--- src/ao.js
+++ src/ao.js
@@ -6,12 +6,26 @@
  * Licensed under the MIT license.
  */
 
-export default function ao(ua = window.navigator.userAgent) {
-    'use strict';
-    // https://support.google.com/webmasters/answer/1061943?hl=en
-    let BOTS_RE=/60Spider|AhrefsBot|Baiduspider|Baiduspider-image|DuckDuckGo-Favicons-Bot|Exabot|Feedfetcher-Google|Feedfetcher-Google|Googlebot|Googlebot|MJ12bot|Mail.RU_Bot|PhantomJS|SAMSUNG-SGH-E250|SafeDNSBot|SemrushBot|Seznam screenshot-generator|SeznamBot|Sogou web spider|Uptimebot|Yahoo! Slurp|YandexBot|bingbot|linkdexbot|spbot/;
 
-    // https://developer.chrome.com/multidevice/user-agent
-    let MOBILES_RE=/iPhone|iPad|iPod|Android/;
-    return ua.search(MOBILES_RE);
-}
+module.exports = (function() {
+  'use strict';
+
+  // https://developer.chrome.com/multidevice/user-agent
+  var MOBILES_RE=/iPhone|iPad|iPod|Android/;
+
+  // https://support.google.com/webmasters/answer/1061943?hl=en
+  var BOTS_RE=/60Spider|AhrefsBot|Baiduspider|Baiduspider-image|DuckDuckGo-Favicons-Bot|Exabot|Feedfetcher-Google|Feedfetcher-Google|Googlebot|Googlebot|MJ12bot|Mail.RU_Bot|PhantomJS|SAMSUNG-SGH-E250|SafeDNSBot|SemrushBot|Seznam screenshot-generator|SeznamBot|Sogou web spider|Uptimebot|Yahoo! Slurp|YandexBot|bingbot|linkdexbot|spbot/;
+
+  var is = function(re) {
+    return function(ua) {
+      var ua = ua ? ua : window.navigator.userAgent;
+      return ua.search(re) !== -1;
+    };
+  };
+
+  return {
+    isMobile: is(MOBILES_RE),
+    isBot: is(BOTS_RE)
+  }
+
+}());
